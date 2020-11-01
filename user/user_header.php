941d32e33152f143b7d0bf27fe53925633f0b669
Birthday4z <werapat.bird@gmail.com>
2020-11-02 04:12:37 +0700

    add datepicker

--- user/user_header.php
+++ user/user_header.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="/project/src/css/bootstrap.min.css">
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <div class="navbar-header">

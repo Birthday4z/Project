e30e145954f144d5bb32470b1861f9054d98c1e6
Birthday4z <werapat.bird@gmail.com>
2020-11-02 04:22:05 +0700

    add datepicker

--- user/Petition/cleanform.php
+++ user/Petition/cleanform.php
@@ -24,11 +24,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Petition - Cleanform</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/project/src/css/bootstrap.min.css">
+    <script src="/project/src/js/jquery-3.3.1.min.js"></script>
+    <script src="/project/src/js/bootstrap.min.js"></script>
     
 </head>
 <body>
     <?php include_once "../user_header.php" ?>
+
+    <link rel="stylesheet" href="/project/src/css/datepicker.css">
+    <script src="/project/src/js/bootstrap-datepicker.js"></script>    
+    <script>
+        $(document).ready(function() {
+            $('#want_date').datepicker({
+                format: "yyyy-mm-dd",
+                immediateUpdates: true,
+                todayBtn: true,
+                todayHighlight: true,
+                autoclose: true
+            })
+        });
+    </script>
+
     <div class="container">
         <form method="POST">
         <h1 class="mt-5">คำร้องทำความสะอาดห้องพัก</h1> 
@@ -41,11 +59,11 @@
         <h5>เบอร์ติดต่อ : <?php echo $_SESSION['Phone'];?></h5>
         <br>
         <h5>วันที่ยื่นคำร้อง : <?php
-                            echo date("Y/m/d");
+                            echo date("Y-m-d");
                             ?>
         </h5>
         <br>
-        <h5>วันที่ต้องการ : <input type="date" name="want_date" required></h5>
+        <h5>วันที่ต้องการ : <input type="text" name="want_date" id="want_date" required placeholder="Select Date ..."></h5>
         <br>
 
 
@@ -75,12 +93,7 @@
 
 
 
-    
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-</body>
+    </body>
 </html>
 
 
